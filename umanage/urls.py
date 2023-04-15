b49b21c151454fe90eae3e58eb39de9cd4546d91
Raphael Laaz Muñoz <raphaellaaz01@gmail.com>
2023-04-15 18:39:57 -0500

    Cambios realizados: 	-Urls-> Adicion de rutas de otras paginas (recovery y register) 	-Templates-> Adicion de archivo login.html 	-Views-> Adicion de las funciones que respondan a los request respectivos Eso es todo en este commit

--- umanage/urls.py
+++ umanage/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.login)
+    path('', views.login, name='login')
+    path('register/', views.register, name='register'),
+    path('recovery/', views.recovery, name='recovery'),
 ]
